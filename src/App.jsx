dfe332fb0c675925cbf092d1ac534a46d160e874
Jay <jay.sanjay.karia@gmail.com>
2023-08-09 20:41:08 +0530

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import * as React from "react"
 import { useState } from 'react'
 
-function App() {
+import { AppBar } from '@mui/material'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material'
+import Button from '@mui/material'
+import Box from '@mui/material'
 
-  return (
-    <>
-     
-    </>
-  )
+import Navbar from './Components/Navbar'
+
+function App() {
+  return <>
+    <Navbar/>
+  </>
 }
 
 export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar

1ea72cda2e351cfb1fcb90cefbe33f25008009e0
Jay <jay.sanjay.karia@gmail.com>
2023-08-22 16:43:50 +0530

    responsive BANNER!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import "./App.css"
 
 import HomeScreen from "./Components/HomeScreen/HomeScreen"
 
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,6 +4,10 @@ import './Banner.css'
 import requests from '../api/requests'
 import axios from 'axios'
 
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+import Skeleton from '@mui/material/Skeleton'
+import Box from '@mui/material/Box'
+
 const Banner = () => {
   const [movies, setMovies] = useState([])
   const [index, setIndex] = useState(0)
@@ -17,7 +21,7 @@ const Banner = () => {
   }
 
   const update = (max) => {
-    max--;
+    max--
     if (index >= max) {
       setIndex(0)
     } else {
@@ -29,8 +33,10 @@ const Banner = () => {
     const fetchPopular = async () => {
       const request = await axios.get(requests.fetchPopular) // popular movie
       let arr = []
+      let p = 0
       for (let i = 0; i < 5; i++) {
-        arr.push(request.data.results[i])
+        let num = Math.floor(Math.random() * request.data.results.length)
+        arr.push(request.data.results[num])
       }
       setMovies(arr)
       return request
@@ -40,35 +46,54 @@ const Banner = () => {
 
   return (
     <>
-      <header
-        className="banner"
-        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${movies[index].backdrop_path})` }}
-      >
-        <div className="banner-content">
-          <div
-            className="right"
-            onClick={() => {
-              update(5)
-            }}
-          >
-            {'>'}
-          </div>
-          <div className="header">
-            <h1>{movies[index].original_title}</h1>
-          </div>
-          <div className="buttons">
-            <button className="btn btn-1">
-              <b>Play</b>
-            </button>
+      {movies.length > 0 ? (
+        <header
+          className="banner"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${movies[index].backdrop_path})`,
+            backgroundPosition: 'center center',
+            backgroundSize: '100% 100%',
+          }}
+        >
+          <div className="banner-content">
+            <div
+              className="right"
+              onClick={() => {
+                update(5)
+              }}
+            >
+              <ArrowForwardIosIcon />
+            </div>
+            <div className="header">
+              <h1>{movies[index].original_title}</h1>
+            </div>
+            <div className="buttons">
+              <button className="btn btn-1">
+                <b>Play</b>
+              </button>
 
-            <button className="btn btn-2">
-              <b>My List</b>
-            </button>
+              <button className="btn btn-2">
+                <b>My List</b>
+              </button>
+            </div>
+            <div className="description">
+              {trim(movies[index].overview, 200)}
+            </div>
           </div>
-          <div className="description">{trim(movies[index].overview, 200)}</div>
-        </div>
-        <div className="fade"></div>
-      </header>
+          <div className="fade"></div>
+        </header>
+      ) : (
+        <>
+          {/* Skeleton */}
+            <Skeleton
+              sx={{ bgcolor: 'grey.900' }}
+              variant="rectangular"
+              width={210}
+              height={118}
+              style={{height:"35rem", width:"100%"}}
+            />
+        </>
+      )}
     </>
   )
 }

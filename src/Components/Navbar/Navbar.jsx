4521b06b772c1d05da84544ac9fdb6088780ac8f
Jay <jay.sanjay.karia@gmail.com>
2023-08-26 02:36:38 +0530

    final touches done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,41 +1,32 @@
-// import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
-// import './Navbar.css'
-
-// const Navbar = () => {
-
-//   const [show, setShow] = useState(false)
-
-//   const transitionNavbar = ()=> {
-//     if (window.scrollY > 100) {
-//       setShow(true)
-//     } else {
-//       setShow(false)
-//     }
-//   }
-
-//   useEffect(() => {
-//     window.addEventListener("scroll", transitionNavbar)
-//     return () => window.removeEventListener("scroll", transitionNavbar)
-//   }, [])
-  
-
-//   return (
-//     <div className={`navbar ${!show ?'navbar-black': ''}`}>
-//       <div className="nav-contents">
-//         <img className="logo" src="/netflix.png" alt="logo" />
-//         <img className="avatar" src="/profile.png" alt="avatar" />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
+
+  const [show, setShow] = useState(false)
+
+  const transitionNavbar = ()=> {
+    if (window.scrollY > 100) {
+      setShow(true)
+    } else {
+      setShow(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar)
+    return () => window.removeEventListener("scroll", transitionNavbar)
+  }, [])
+  
+
   return (
-    <div>Navbar</div>
+    <div className={`navbar ${!show ?'navbar-black': ''}`}>
+      <div className="nav-contents">
+        <img className="logo" src="/netflix.png" alt="logo" />
+        <img className="avatar" src="/profile.png" alt="avatar" />
+      </div>
+    </div>
   )
 }
 
